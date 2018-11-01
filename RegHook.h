b174443322d4783a5e3dd67d238f6050cd988566
coltonon <cnpearson42@gmail.com>
2018-11-01 02:08:42 -0500

    Updated Stuff

--- RegHook.h
+++ RegHook.h
@@ -5,15 +5,23 @@
 class RegDump
 {
 public:
-	char pad_0000[88]; //0x0000
-	DWORD_PTR RBX; //0x0058
-	DWORD_PTR RSP; //0x0060
-	DWORD_PTR RDI; //0x0068
-	DWORD_PTR RSI; //0x0070
-	DWORD_PTR RBP; //0x0078
-	DWORD_PTR RDX; //0x0080
-	DWORD_PTR RCX; //0x0088
-	DWORD_PTR RAX; //0x0090
+	char pad_0000[160]; //0x0000
+	DWORD_PTR R15; //0x00A0
+	DWORD_PTR R14; //0x00A8
+	DWORD_PTR R13; //0x00B0
+	DWORD_PTR R12; //0x00B8
+	DWORD_PTR R11; //0x00C0
+	DWORD_PTR R10; //0x00C8
+	DWORD_PTR R9; //0x00D0
+	DWORD_PTR R8; //0x00D8
+	DWORD_PTR RDI; //0x00E0
+	DWORD_PTR RSI; //0x00E8
+	DWORD_PTR RSP; //0x00F0
+	DWORD_PTR RBP; //0x00F8
+	DWORD_PTR RDX; //0x0100
+	DWORD_PTR RCX; //0x0108
+	DWORD_PTR RBX; //0x0110
+	DWORD_PTR RAX; //0x0118
 };
 
 class RegHook {
@@ -40,11 +48,11 @@ private:
 	static std::vector<RegHookEx*> HookInstances;
 	HANDLE hProcess;
 	DWORD_PTR FuncAddress;
-	size_t lengthOfInstructions;
 	DWORD_PTR HookedAddress = 0;
 	byte toFixPatch[60];
 	bool CreateHookV6();
 	size_t GetFuncLen();
+	size_t lengthOfInstructions;
 public:
 	RegHookEx(HANDLE _hProcess, DWORD_PTR _FuncAddress);
 	DWORD_PTR GetAddressOfHook();
